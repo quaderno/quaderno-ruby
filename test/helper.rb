e2249d1af5c6b8b1bbde9d9ed3e04264c8908187
xxswingxx <vidadelaempresa@gmail.com>
2014-02-04 14:23:36 +0000

    Removed old cassettes

--- test/helper.rb
+++ test/helper.rb
@@ -1,26 +1,27 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'rubygems'
 require 'bundler'
+
+begin
+  Bundler.setup(:default, :test)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+
 require 'vcr'
+require 'test/unit'
+require 'shoulda'
+require 'quaderno-ruby'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'test/fixtures/quaderno_cassettes'
   c.ignore_localhost = false
   c.hook_into :fakeweb
 end
-
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-require 'test/unit'
-require 'shoulda'
-
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'quaderno-ruby'
 
 class Test::Unit::TestCase
 end
