7f1698be8739feb3d663e78a2d0cf1aef6578b26
xxswingxx <vidadelaempresa@gmail.com>
2017-09-13 14:16:47 +0100

    Version bump to 1.13.0

--- lib/quaderno-ruby.rb
+++ lib/quaderno-ruby.rb
@@ -5,7 +5,7 @@ require 'quaderno-ruby/helpers/authentication'
 require 'quaderno-ruby/collection'
 
 %w(block crud deliver payment retrieve).each { |filename| require "quaderno-ruby/behavior/#{filename}" }
-%w(base contact item invoice receipt credit income estimate expense recurring document_item evidence payment webhook tax).each { |filename| require "quaderno-ruby/#{ filename }" }
+%w(base contact item invoice receipt credit income estimate expense recurring document_item report evidence payment webhook tax).each { |filename| require "quaderno-ruby/#{ filename }" }
 
 module Quaderno
 
---
+++ lib/quaderno-ruby/report.rb
@@ -0,0 +1,25 @@
+module Quaderno
+  class Report < Base
+    api_model Quaderno::Report
+    api_path 'reports'
+
+    class << self
+      undef :all, :find, :create, :update, :delete, :parse_nested
+    end
+
+    def self.journal(options = {})
+      authentication = get_authentication(options.merge(api_model: api_model))
+      filter = options.delete_if { |k,v| %w(auth_token access_token api_url mode api_model).include? k.to_s }
+
+      response = get("#{authentication[:url]}#{api_model.api_path}/journal.json",
+        query: filter,
+        basic_auth: authentication[:basic_auth],
+        headers: version_header.merge(authentication[:headers])
+      )
+
+      check_exception_for(response, { rate_limit: true, subdomain_or_token: true })
+
+      response.parsed_response
+    end
+  end
+end
