a5d8452ccc905742503e6c51cf94f2b69c4e87fb
xxswingxx <vidadelaempresa@gmail.com>
2017-12-22 11:05:00 +0000

    Load Quaderno class

--- lib/quaderno-ruby.rb
+++ lib/quaderno-ruby.rb
@@ -1,3 +1,6 @@
+class Quaderno
+end
+
 require 'ostruct'
 
 require 'quaderno-ruby/exceptions/exceptions'
@@ -6,6 +9,3 @@ require 'quaderno-ruby/collection'
 
 %w(block crud deliver payment retrieve).each { |filename| require "quaderno-ruby/behavior/#{filename}" }
 %w(base contact item invoice receipt credit income estimate expense recurring document_item report evidence payment webhook tax).each { |filename| require "quaderno-ruby/#{ filename }" }
-
-class Quaderno
-end
