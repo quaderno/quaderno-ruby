1bdfb1e13b71a0fac47f598b4839814ac1ce3080
Nick Coyne <nick@codevader.com>
2021-07-30 06:14:08 +1200

    Add report requests endpoint

--- lib/quaderno-ruby.rb
+++ lib/quaderno-ruby.rb
@@ -10,4 +10,4 @@ require 'quaderno-ruby/helpers/authentication'
 require 'quaderno-ruby/collection'
 
 %w(block crud deliver payment retrieve).each { |filename| require "quaderno-ruby/behavior/#{filename}" }
-%w(base contact item invoice receipt credit income estimate expense recurring document_item report evidence payment webhook tax checkout_session).each { |filename| require "quaderno-ruby/#{ filename }" }
+%w(base contact item invoice receipt credit income estimate expense recurring document_item report report_request evidence payment webhook tax checkout_session).each { |filename| require "quaderno-ruby/#{ filename }" }
---
+++ lib/quaderno-ruby/report_request.rb
@@ -0,0 +1,4 @@
+class Quaderno::ReportRequest < Quaderno::Base
+  api_model Quaderno::ReportRequest
+  api_path 'reporting/requests'
+end
