9a77510c56cf3861af6b40e6005edca9c37f895c
xxswingxx <vidadelaempresa@gmail.com>
2021-09-06 13:09:48 +0100

    Return next page cursor

--- lib/quaderno-ruby/collection.rb
+++ lib/quaderno-ruby/collection.rb
@@ -21,6 +21,12 @@ class Quaderno::Collection < Array
     @has_more == 'true'
   end
 
+  def next_page_cursor
+    return unless @next_page_url
+
+    @next_page_url.to_s.match(/created_before=(\d+)/)&.[](1)
+  end
+
   def next_page
     return Quaderno::Collection.new unless has_more?
     @collection_type.all_from_url(@next_page_url, @request_options)
